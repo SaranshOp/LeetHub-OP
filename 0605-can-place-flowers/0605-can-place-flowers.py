16c75e4c993b7dc9432111a3b8e6a1371ea4a762
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-07-21 20:35:00 +0530

    Time: 131 ms (60.23%), Space: 16.8 MB (84.87%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,16 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n==0:
+            return True
         length = len(flowerbed)
-        
         for i in range(length):
-            if (flowerbed[i] == 0 and  # Check if current plot is empty
-                (i == 0 or flowerbed[i - 1] == 0) and  # Check if it's the first plot or the previous plot is empty
-                (i == length - 1 or flowerbed[i + 1] == 0)):  # Check if it's the last plot or the next plot is empty
-                
-                flowerbed[i] = 1  # Plant a flower here
-                n -= 1  # Decrease the count of flowers to plant
-                
-                if n == 0:  # If no more flowers need to be planted
+            if(flowerbed[i] == 0 and (i ==0 or flowerbed[i-1] == 0) and ( i == length -1 or flowerbed[i+1] ==0)):
+                flowerbed[i]  = True
+                n-=1
+                if n==0:
                     return True
-        
-        return n <= 0  # Check if all flowers have been planted
+        return False   
