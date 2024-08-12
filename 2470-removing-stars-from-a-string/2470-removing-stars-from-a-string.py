337b619771facd118bec097590d4d0f18453abab
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-08-13 03:43:29 +0530

    Time: 139 ms (57.17%), Space: 18.3 MB (16.16%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -1,11 +1,20 @@
 class Solution:
     def removeStars(self, s: str) -> str:
-        stack=[]
-        
-        for i in range(0,len(s)):
-            if s[i]!='*':
-                stack.append(s[i])
+        res = ""
+        to_remove = 0
+        # Represents number of symbols we will skip
+        # when meet
+        for symb in s[::-1]:
+            if symb == "*":
+                to_remove += 1
+                # Skip one more symbol when meet
             else:
-                stack.pop()
-       
-        return  "".join(stack)
+                if not to_remove:
+                # if to_remove == 0
+                    res += symb
+                else:
+                    to_remove -= 1
+                    # Skipping a symbol
+        return res[::-1]
+        # The recieved string is also reversed, so
+        # reverse it to get the answer
