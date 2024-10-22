63df63553375bc641f6a75151cd52121a16ac696
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-22 16:06:27 +0530

    Time: 58 ms (62.61%), Space: 51.3 MB (81.24%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -2,18 +2,29 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    const resetv = init
-    return {
-        increment: ()=>++init,
-        decrement: ()=>--init,
-        reset: ()=> init = resetv,
-    }
-};
+class Counter {
+  constructor(init) {
+    this.init = init;
+    this.presentCount = init;
+  }
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
+  increment() {
+    this.presentCount += 1;
+    return this.presentCount;
+  }
+
+  decrement() {
+    this.presentCount -= 1;
+    return this.presentCount;
+  }
+
+  reset() {
+    this.presentCount = this.init;
+    return this.presentCount;
+  }
+}
+
+var createCounter = function(init) {
+C =  new Counter(init)
+  return C;
+};
