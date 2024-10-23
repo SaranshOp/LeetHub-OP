e7f3e57964c3586f57cbf958d5d1e6f1f3e2fbde
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-23 06:02:01 +0530

    Time: 240 ms (63.16%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
-select
-ifnull((select distinct salary
-from Employee
-order by salary desc
-limit 1 offset 1), null) as SecondHighestSalary
+select max(salary) as SecondHighestSalary
+from employee 
+where salary < (select max(salary) from Employee)
+
+
+-- select
+-- ifnull((select distinct salary
+-- from Employee
+-- order by salary desc
+-- limit 1 offset 1), null) as SecondHighestSalary
 
 -- import pandas as pd
 
