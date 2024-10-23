324a3b8541e9c9cd737102ee4353bcb84efd2594
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-23 06:00:02 +0530

    Time: 241 ms (61.96%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+select
+ifnull((select distinct salary
+from Employee
+order by salary desc
+limit 1 offset 1), null) as SecondHighestSalary
+
+-- import pandas as pd
+
+-- def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+--     newsalary = employee.sort_values(by = "salary", ascending=False).drop_duplicates(subset="salary", keep='first')
+--     if  employee["salary"].nunique() < 2:
+--         salary=pd.DataFrame({'SecondHighestSalary':[pd.NA]})
+--     else:
+--         p = newsalary.iloc[1]["salary"]
+--         salary=pd.DataFrame({'SecondHighestSalary':[p]})
+--     return salary    
+    
