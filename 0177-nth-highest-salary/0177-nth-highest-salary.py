2083569ef7a58f174e815e4cb8a6f1b1b7dd3b0f
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-23 06:49:35 +0530

    Time: 347 ms (73.3%), Space: 70.1 MB (28.02%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    n_employee = employee.drop_duplicates(subset=["salary"], inplace=False)
+    n_employee = employee[["salary"]].drop_duplicates(inplace=False)
     n_employee = n_employee.sort_values(by = "salary" , inplace = False, ascending = False)
     n_employee = n_employee.rename(columns = {"salary":f"getNthHighestSalary({N})"}, inplace = False)
     if n_employee.shape[0] < N or N <= 0:
