9c757128ae7b6c1d695aafde2fa53e064e9f32cc
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-23 06:07:14 +0530

    Time: 329 ms (78.51%), Space: 70.3 MB (16.73%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    newsalary = employee.sort_values(by = "salary", ascending=False).drop_duplicates(subset="salary", keep='first')
+    if  employee["salary"].nunique() < 2:
+        salary=pd.DataFrame({'SecondHighestSalary':[pd.NA]})
+    else:
+        p = newsalary.iloc[1]["salary"]
+        salary=pd.DataFrame({'SecondHighestSalary':[p]})
+    return salary    
+    
+# def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+#     employee.drop_duplicates(subset=["salary"], inplace=True)
+#     if employee.shape[0] < 2 :
+#         return pd.DataFrame([[None]], columns=["SecondHighestSalary"])
+#     employee.sort_values(by="salary", inplace=True, ascending=False)
+#     employee = employee[["salary"]]
+#     employee.rename(columns={"salary" : "SecondHighestSalary"}, inplace=True)
+#     return employee.iloc[1:2]
+    
