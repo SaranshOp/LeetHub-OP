75e1bd126004fbfeb7efb8828536eb5e531813be
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-29 19:27:03 +0530

    Time: 37 ms (32.61%), Space: 74.4 MB (43.37%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
-
-        int dirs[3] = {-1, 0, 1};
         int M = grid.size(), N = grid[0].size();
 
-        queue<vector<int>> q;
-        vector<vector<int>> vis(M, vector<int>(N, 0));
-        // Enqueue the cells in the first column.
+        vector<vector<int>> dp(M, vector<int>(N, 0));
+        // Cells in the first column will have the moves as 1.
+        // This is required to ensure we have a way if the cell reachable or not
+        // from the first column.
         for (int i = 0; i < M; i++) {
-            vis[i][0] = 1;
-            q.push({i, 0, 0});
+            dp[i][0] = 1;
         }
 
         int maxMoves = 0;
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                vector<int> v = q.front();
-                q.pop();
-
-                // Current cell with the number of moves made so far.
-                int row = v[0], col = v[1], count = v[2];
-
-                maxMoves = max(maxMoves, count);
-
-                for (int dir : dirs) {
-                    // Next cell after the move.
-                    int newRow = row + dir, newCol = col + 1;
-
-                    // If the next cell isn't visited yet and is greater than
-                    // the current cell value. Add it to the queue with the
-                    // moves required.
-                    if (newRow >= 0 && newCol >= 0 && newRow < M &&
-                        newCol < N && !vis[newRow][newCol] &&
-                        grid[row][col] < grid[newRow][newCol]) {
-                        vis[newRow][newCol] = 1;
-                        q.push({newRow, newCol, count + 1});
-                    }
+        for (int j = 1; j < N; j++) {
+            for (int i = 0; i < M; i++) {
+                // Check all the three next possible cells
+                // Check if the next cell is greater than the previous one
+                // Check if the previous cell was reachable, if the value is > 0
+                if (grid[i][j] > grid[i][j - 1] && dp[i][j - 1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1);
                 }
+                if (i - 1 >= 0 && grid[i][j] > grid[i - 1][j - 1] &&
+                    dp[i - 1][j - 1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+                if (i + 1 < M && grid[i][j] > grid[i + 1][j - 1] &&
+                    dp[i + 1][j - 1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 1);
+                }
+
+                maxMoves = max(maxMoves, dp[i][j] - 1);
             }
         }
 
