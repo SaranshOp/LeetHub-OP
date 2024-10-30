14d737722ad74de3fecfb5d5530461ac52516739
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-10-30 19:33:22 +0530

    Time: 0 ms (100%), Space: 13.1 MB (68.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,17 +2,32 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // PREFIXSUM concept DP
-        vector<int> dp(n,1);
-        for(int i=0; i<n; i++){
-            int p = nums[i];
-            for(int j=0;j<i;j++){
-                int o = nums[j];
+        vector<int> res;
 
-                if(o < p && dp[i] < dp[j] + 1)
-                dp[i] = dp[j] + 1;
+        for (auto i : nums) {
+            if (res.empty()) {
+                res.push_back(i);
+                continue;
+            }
+            auto it = res.end() - 1;
+            if (i > *it)
+                res.push_back(i);
+            else {
+                auto min = lower_bound(res.begin(), res.end(), i);
+                *min = i;
             }
         }
-        return *max_element(dp.begin(), dp.end());
+        return res.size();
     }
 };
+
+// class Solution:  # 68 ms, faster than 93.92%
+//     def lengthOfLIS(self, nums: List[int]) -> int:
+//         sub = []
+//         for x in nums:
+//             if len(sub) == 0 or sub[-1] < x:
+//                 sub.append(x)
+//             else:
+//                 idx = bisect_left(sub, x)  # Find the index of the first element >= x
+//                 sub[idx] = x  # Replace that number with x
+//         return len(sub)
