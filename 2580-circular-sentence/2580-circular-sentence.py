6fc12c5693ae38c93133e93761db5568e8697064
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-11-02 20:45:06 +0530

    Time: 0 ms (100%), Space: 16.6 MB (11.84%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        sentence = sentence.split()
+        for i in range(len(sentence)):
+            if sentence[i-1][-1] != sentence[i][0]:
+                return False
+        return True
