3585811314f5b211f6acd26851b18d6a342d7f73
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-11-14 02:04:18 +0530

    Time: 0 ms (100%), Space: 28.5 MB (41.54%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,79 +1,43 @@
-class Solution
-{
+class Solution {
 public:
-    bool primeSubOperation(vector<int> &nums)
-    {
-        // Generate a list of prime numbers up to 1000
-        vector<int> primes;
-        sieve(primes, 1000); // Helper function to fill the primes vector
+    bool primeSubOperation(vector<int>& nums) {
+        int maxElement = *max_element(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            // Find the largest prime number less than or equal to nums[i]
-            int j = lower_bound(primes.begin(), primes.end(), nums[i]) - primes.begin() - 1;
-            if(j==-1) continue;
-            cout << "i: " << i << " " ;
-            cout << "nums[i]: " << nums[i] << " ";
-            cout << "j: " << j << " ";
-            cout << "primes[j]: " << primes[j] << " ";
-            cout << endl;
-            // Adjust the first element directly
-            if (i == 0)
-            {
-                nums[i] -= primes[j]; // Subtract the largest prime from the first element
-                cout << " Fnums[i]:s " << nums[i] << endl;  
-                continue;
-            } // initial j
-            // Find a suitable prime such that nums[i] - primes[j] is less than nums[i-1]
-            while (j>=0 && nums[i - 1] >= nums[i] - primes[j])
-            {
-                j--; // Move to the next smaller prime
-            }
-            if(j<0 && nums[i-1] < nums[i]) continue;
-            cout << " HERE!Outside " << endl;
-            // If no valid prime can be subtracted, return false
-            if (j < 0 || nums[i] <= primes[j]){
-                cout << " HERE!Inside ";
-                return false;
-            }
-
-            // Subtract the valid prime from the current element
-            nums[i] -= primes[j];
-
-            cout << " Fnums[i]: " << nums[i] << endl;  
-        }
-
-        // Check if the resulting array is sorted in ascending order
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] <= nums[i - 1]){
-                cout<<"false here";
-                return false;
-                }
-        }
-
-        return true; // All operations were successful and the array is sorted
-    }
-private:
-    void sieve(vector<int> &primes, int n)
-    {
-        // Sieve of Eratosthenes to generate all prime numbers up to n
-        vector<bool> isPrime(n + 1, true);
-        for (int p = 2; p * p <= n; p++)
-        {
-            if (isPrime[p])
-            {
-                for (int i = p * p; i <= n; i += p)
-                {
-                    isPrime[i] = false;
+        // Store the sieve array.
+        vector<int> sieve(maxElement + 1, 1);
+        sieve[1] = 0;
+        for (int i = 2; i <= sqrt(maxElement + 1); i++) {
+            if (sieve[i] == 1) {
+                for (int j = i * i; j <= maxElement; j += i) {
+                    sieve[j] = 0;
                 }
             }
         }
-       // primes.push_back(1);
-        for (int p = 2; p <= n; p++)
-        {
-            if (isPrime[p])
-                primes.push_back(p); // Add prime numbers to the list
+
+        // Start by storing the currValue as 1, and the initial index as 0.
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.size()) {
+            // Store the difference needed to make nums[i] equal to currValue.
+            int difference = nums[i] - currValue;
+
+            // If difference is less than 0, then nums[i] is already less than
+            // currValue. Return false in this case.
+            if (difference < 0) {
+                return 0;
+            }
+
+            // If the difference is prime or zero, then nums[i] can be made
+            // equal to currValue.
+            if (sieve[difference] == 1 or difference == 0) {
+                i++;
+                currValue++;
+            } else {
+                // Otherwise, try for the next currValue.
+                currValue++;
+            }
         }
+        return 1;
     }
 };
+
