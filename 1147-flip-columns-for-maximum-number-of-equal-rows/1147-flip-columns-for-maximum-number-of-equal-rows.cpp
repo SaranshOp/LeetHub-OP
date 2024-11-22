ede4f7c6473c603ff34065c5b55b374acd0e5e2f
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-11-22 22:57:42 +0530

    Time: 63 ms (31.88%), Space: 97.9 MB (5.6%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,24 +1,27 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();  
-        unordered_map<string, int> matrixMap;  
+        int m = matrix.size(), n = matrix[0].size(); 
+        map<vector<int>, int> matrixMap; 
 
-        for (int i = 0; i < m; ++i) {  
-            
-            string original = "", flipped = "";
-            for (int j = 0; j < n; ++j) {
-                original += to_string(matrix[i][j]);  
-                flipped += to_string(1 - matrix[i][j]);
+        for (int i = 0; i < m; i++) { 
+            vector<int> vc1, vc0;
+            for (int j = 0; j < n; j++) { 
+                int e = matrix[i][j];
+                if (e == 0) vc0.push_back(j); 
+                if (e == 1) vc1.push_back(j);
             }
-
-          
-            matrixMap[original]++;
-            matrixMap[flipped]++;
+            matrixMap[vc0]++; 
+            matrixMap[vc1]++; 
         }
 
-        int maxi = 0;  
-        for (auto it = matrixMap.begin(); it != matrixMap.end(); ++it) {
+        int maxi = INT_MIN;
+        for (auto it = matrixMap.begin(); it != matrixMap.end(); it++) {
             maxi = max(maxi, it->second);
         }
 
