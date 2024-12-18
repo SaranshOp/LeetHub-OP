117161658147f5dda5c5fc6d9c50cb70c5952e69
Saransh <77978788+SaranshOp@users.noreply.github.com>
2024-12-18 11:02:31 +0530

    Time: 7 ms (61.99%), Space: 12.5 MB (38.16%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        // Create a map of strings to store the count of all substrings.
         map<string, int> count;
         for (int start = 0; start < s.length(); start++) {
             string currString;
             for (int end = start; end < s.length(); end++) {
-                // If the string is empty, or the current character is equal to
-                // the previously added character, then add it to the map.
-                // Otherwise, break the iteration.
-                if (currString.empty() or currString.back() == s[end]) {
+                if (currString.empty() || currString.back() == s[end]) {
                     currString.push_back(s[end]);
                     count[currString]++;
                 } else {
@@ -17,9 +13,6 @@ public:
                 }
             }
         }
-
-        // Create a variable ans to store the longest length of substring with
-        // frequency atleast 3.
         int ans = 0;
         for (auto i : count) {
             string str = i.first;
