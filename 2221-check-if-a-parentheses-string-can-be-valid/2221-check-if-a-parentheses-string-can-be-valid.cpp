5deb9fadc148a3d8adebae21eb4de7d70b74d672
Saransh <77978788+SaranshOp@users.noreply.github.com>
2025-02-07 03:14:47 +0530

    Time: 16 ms (36.45%), Space: 30.1 MB (77.1%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -6,34 +6,41 @@ public:
         if (length % 2 == 1) {
             return false;
         }
-
-        stack<int> openBrackets, unlocked;
-
-        // Iterate through the string to handle '(' and ')'
+        int openBrackets = 0, unlocked = 0;
+        // Iterate through the string to handle '(' and ')'.
         for (int i = 0; i < length; i++) {
             if (locked[i] == '0') {
-                unlocked.push(i);
+                unlocked++;
             } else if (s[i] == '(') {
-                openBrackets.push(i);
+                openBrackets++;
             } else if (s[i] == ')') {
-                if (!openBrackets.empty()) {
-                    openBrackets.pop();
-                } else if (!unlocked.empty()) {
-                    unlocked.pop();
+                if (openBrackets > 0) {
+                    openBrackets--;
+                } else if (unlocked > 0) {
+                    unlocked--;
                 } else {
                     return false;
                 }
             }
         }
-
-        // Match remaining open brackets with unlocked characters
-        while (!openBrackets.empty() && !unlocked.empty() &&
-               openBrackets.top() < unlocked.top()) {
-            openBrackets.pop();
-            unlocked.pop();
+        // Match remaining open brackets with unlocked characters.
+        int balance = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (locked[i] == '0') {
+                balance--;
+                unlocked--;
+            } else if (s[i] == '(') {
+                balance++;
+                openBrackets--;
+            } else if (s[i] == ')') {
+                balance--;
+            }
+            if (balance > 0) {
+                return false;
+            }
         }
 
-        if (!openBrackets.empty()) {
+        if (openBrackets > 0) {
             return false;
         }
 
