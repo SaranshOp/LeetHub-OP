4e0f94a9a2e2f040c19834de2ef2886bbff2e362
Saransh <77978788+SaranshOp@users.noreply.github.com>
2025-02-07 13:24:08 +0530

    Time: 8 ms (15.72%), Space: 18.1 MB (65.89%) - LeetHub

---
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.py
@@ -0,0 +1,40 @@
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def minCameraCover(self, root: Optional[TreeNode]) -> int:
+#         covered = {None}
+#         self.ans = 0
+#         def dfs(node, par = None): 
+#             if (node == None): 
+#                 return
+#             else:
+#                 dfs(node.left, node)
+#                 dfs(node.right, node)
+#             if((par == None or par not in covered) and (node.left not in covered or node.right not in covered)):
+#                 self.ans += 1
+#                 covered.update({node, par, node.left, node.right})
+            
+#         dfs(root)
+#         return self.ans; 
+
+class Solution(object):
+    def minCameraCover(self, root):
+        self.ans = 0
+        covered = {None}
+
+        def dfs(node, par = None):
+            if node:
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+                if (par is None and node not in covered or
+                        node.left not in covered or node.right not in covered):
+                    self.ans += 1
+                    covered.update({node, par, node.left, node.right})
+
+        dfs(root)
+        return self.ans
