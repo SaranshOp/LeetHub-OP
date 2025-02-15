9b9b159202c0efad9646d3d2a392e02a4472ac33
Saransh <77978788+SaranshOp@users.noreply.github.com>
2025-02-15 17:50:52 +0530

    Time: 141 ms (31.61%), Space: 78 MB (76.59%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,36 +1,36 @@
 class ProductOfNumbers {
 private:
-    // Stores cumulative product of the stream
-    vector<int> prefixProduct;
-    int size = 0;
-
+    vector<int> prod;
+    int size=0;
 public:
     ProductOfNumbers() {
-        // Initialize the product list with 1 to handle multiplication logic
-        prefixProduct.push_back(1);
-        size = 0;
+    prod = {1};
+    
     }
 
     void add(int num) {
         if (num == 0) {
-            // If num is 0, reset the cumulative products since multiplication
-            // with 0 invalidates previous products
-            prefixProduct = {1};
+            prod = {1};
             size = 0;
-        } else {
-            // Append the cumulative product of the current number with the last
-            // product
-            prefixProduct.push_back(prefixProduct[size] * num);
-            size++;
+            }
+        else{
+            size = prod.size();
+            prod.push_back(prod[size-1] *  num);
         }
+        size++;
     }
-
+    
     int getProduct(int k) {
-        // Check if the requested product length exceeds the size of the valid
-        // product list
-        if (k > size) return 0;
-
-        // Compute the product of the last k elements using division
-        return prefixProduct[size] / prefixProduct[size - k];
+        if(k>size-1) return 0;
+        cout << "sz " << size << endl;
+        cout << "prod " << prod[size - 1] << endl;
+        return prod[size-1]/prod[size-k-1];
     }
 };
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
